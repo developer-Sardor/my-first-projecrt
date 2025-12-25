50a104a085b4a8eb6b74de8aebf89cd8b62c17aa
developer-Sardor <mominjonovsardor3@gmail.com>
2025-12-25 15:25:43 +0900

    first push

---
+++ index.js
@@ -0,0 +1,53 @@
+                    //Vazifa
+//1-masala  Onam ro'zg'or uchun xarid qilish kerak bo'lgan narsalar ro'yxatini tuzib berdi.
+//unga ko'ra men bozordan oshUchun:"guruch, sabzi,piyoz", mantiUchun:"1kg go'sht", dimlamaUchun:"kartoshka, no'xot"
+//olib kelishim kerak. men yarim yo'ldaligimda onam telefon qilin uyga mehmonlar kelyapti, mantiuchun go'shtni
+//1kg emas 2 kg ol deb aytdi. 1 kg go'shtni 2kg go'shtga almashtirib bering.
+// let shopping ={oshUchun:"guruch, sabzi,piyoz", mantiUchun:"1kg go'sht", dimlamaUchun:"kartoshka, no'xot"}
+// console.log(shopping.mantiUchun.replace("1kg go'sht", "2kg go'sht"))
+
+
+//2-Masala. Bir bola 7.5 IELTS oldi va ustozlaridan qaysi universitetga topshirishni maslahat so'radi.
+//ustozi bunday dedi:agar sen 5.5 bal olsang Uzbdagi, 6.0 olsang Koreyadagi, 6.5 olsang Saudiyadagi,
+//7.0 olsang Angliyadagi va 7.5 dan baland bal olsang Amerikadagi univerlarda o'qiy olasan. ustozni gapiga ko'ra 
+//bola qays univerlarda o'qiy oladi?
+
+// function university(IELTS){
+// if (IELTS >=5.5 && IELTS<6.0){
+//     console.log("Uzbdagi Univerlarga kira olasiz")
+// }else if (IELTS>=6.0 &&IELTS<6.5){
+//     console.log("Koreyadagi Univerlarga kira olasiz")
+// }else if (IELTS>=6.5 && IELTS<7.0){
+//     console.log("Saudiyadagi Univerlarga kira olasiz")
+// }else if (IELTS>=7.0 && IELTS<7.5){
+//     console.log("Angliyadagi Univerlarga kira olasiz")
+// }else if (IELTS>=7.5 && IELTS <=9.0){
+//     console.log("Amerikadagi univerlarga kira olasiz")
+// }else {
+//     console.log("Rossiyaga ishlagani keting!!!")
+// }
+// }
+// university(7.5)
+//3-masala quyidagi uzb va chet el ismlari keltirilgan:Ahror, Abbos, Ozod, Bilol;
+//John, Alex, Kim, Peter. noodatiy bo'lishi uchun uzb ismlarig= boshiga  Mr. va chet el ismlari
+//oxriga  bek qo'shimchasini qo'shing.
+
+
+// let uzbIsmlar = ["Ahror", "Abbos", "Ozod", "Bilol"]
+// let foreignIsmlar = ["John", "Alex", "Kim", "Peter"]
+
+//  uzbIsmlar.forEach((n)=> console.log("Mr."+n))
+// foreignIsmlar.forEach((f)=>console.log(f + "bek") )
+
+//4-masala Men bu yil uzbga ketmoqchiman.lek avval boshlig'imdan ruxsat olishim kerak.
+//agar boshligim javobi ha bo'lsa boraman bo'lmasa yana bir yil ishlayman degan javobni 
+//if else ning qisqartmali shaklida chiqarib bering.
+
+// let answer = "no"
+// let result = answer === "yes" ? "Uzbga ketaman" : "Yana bir yil ishlayman"
+// console.log(result)
+
+//5-masala 
+
+
+
